f5c456f51caadbb17adfc5562a3f9881365f0432
toosign <127032516+toosign00@users.noreply.github.com>
2025-03-30 23:35:55 +0900

    [Bronze V] Title: 사칙연산, Time: 96 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const fileData = fs.readFileSync(0).toString().trim().split(' ');
+
+const a = parseInt(fileData[0]);
+const b = parseInt(fileData[1]);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);
