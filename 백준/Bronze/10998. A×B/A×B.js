73b1fa8a413493ce583ff49e40d1bec4744f6e0e
toosign <127032516+toosign00@users.noreply.github.com>
2025-03-30 23:34:57 +0900

    [Bronze V] Title: A×B, Time: 96 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const fileData = fs.readFileSync(0).toString().trim().split(' ');
+
+const a = parseInt(fileData[0]);
+const b = parseInt(fileData[1]);
+
+console.log(a * b);
